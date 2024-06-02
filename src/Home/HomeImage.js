07f78524ac7e28fc4649fe9e8bb6a1f517b35985
pradyutf <pradyutfogla@gmail.com>
2024-06-02 22:42:25 +0530

    pfp bug fix

--- src/Home/HomeImage.js
+++ src/Home/HomeImage.js
@@ -1,7 +1,7 @@
 function HomeImage() {
   return (
     <div className="HomeImage">
-      <img src="http://m.gettywallpapers.com/wp-content/uploads/2023/10/Black-And-White-Anime-Profile-Photo-1.jpg" alt="Profile Pic" id="pfp"/>
+      <img src="https://i.pinimg.com/originals/8d/bc/3f/8dbc3facd0ffd156dfbf5eb42e065ec0.jpg" alt="Profile Pic" id="pfp"/>
     </div>
   );
 }
